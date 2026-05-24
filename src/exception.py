dd0e25048ec10ad5ff8cc839a0c4c2634b37e730
AbhishekG-cloud <mritunjaygoswami5@gmail.com>
2026-05-24 17:53:25 +0530

    Exception file correction

--- src/exception.py
+++ src/exception.py
@@ -15,5 +15,5 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message,error_details=error_details)
     def __str__(self):
         return self.error_message
-
+ 
 
