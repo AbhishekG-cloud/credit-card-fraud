4dec6934153e886c68e0373bb53b9347e738d4aa
AbhishekG-cloud <mritunjaygoswami5@gmail.com>
2026-06-03 19:12:56 +0530

    Application Initial Setup

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+## route for home page 
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/prediction',methods = ["GET","POST"])
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        data = CustomData()
